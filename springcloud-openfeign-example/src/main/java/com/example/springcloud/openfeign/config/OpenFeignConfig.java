71b934e760f7b249ffbc29162b11217c2b085136
zhouxinjian <zhouxinjian@xiaomi.com>
2021-08-20 17:57:44 +0800

    feat:introduce OpenFeign

---
+++ springcloud-openfeign-example/src/main/java/com/example/springcloud/openfeign/config/OpenFeignConfig.java
@@ -0,0 +1,13 @@
+package com.example.springcloud.openfeign.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Williami
+ * @description
+ * @date 2021/8/20
+ */
+@Configuration
+public class OpenFeignConfig {
+
+}
---
+++ springcloud-openfeign-example/src/main/java/com/example/springcloud/openfeign/service/GitHubService.java
@@ -0,0 +1,28 @@
+package com.example.springcloud.openfeign.service;
+
+import com.example.springcloud.openfeign.config.OpenFeignConfig;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Spring容器中的Bean:
+ * FeignClientSpecification{name='githubServiceClient', configuration=[class com.example.springcloud.openfeign.config.OpenFeignConfig]}
+ * JDK代理：HardCodedTarget(type=GitHubService, name=github-client, url=https://api.github.com)
+ *
+ * @author Williami
+ * @description
+ * @date 2021/8/20
+ */
+@FeignClient(name = "github-client", url = "https://api.github.com", configuration = OpenFeignConfig.class, contextId = "githubServiceClient")
+public interface GitHubService {
+
+    /**
+     * @param queryStr
+     * @return
+     */
+    @GetMapping(value = "/search/repositories")
+    String searchRepo(@RequestParam("q") String queryStr);
+
+
+}
