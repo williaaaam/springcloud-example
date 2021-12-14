0d4b237b07290662a8b7e96171fea2cc4ad0fd75
zhouxinjian <zhouxinjian@xiaomi.com>
2021-12-14 18:58:56 +0800

    feat:注入HttpServletRequest源码

--- springcloud-demo-docker/src/main/java/com/example/springcloud/demo/docker/controller/DockerDemoController.java
+++ springcloud-demo-docker/src/main/java/com/example/springcloud/demo/docker/controller/DockerDemoController.java
@@ -3,6 +3,10 @@ package com.example.springcloud.demo.docker.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Proxy;
+
 /**
  * @author Williami
  * @description
@@ -12,8 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("docker")
 public class DockerDemoController {
 
+    @Resource
+    HttpServletRequest httpServletRequest;
+
     @RequestMapping("home")
     public String home() {
+        System.out.println("httpServletRequest ={} instanceOf Proxy is {}" + httpServletRequest + (httpServletRequest instanceof Proxy));
         return "Hello Docker ~";
     }
 
